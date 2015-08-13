a0de041c63581b686bc380d566e9fd840d882ea6
mob8607 <mathias.ober@massiveart.com>
2015-08-13 23:48:40 +0200

    finished first version

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,11 +1,46 @@
 $(document).ready(function() {
-    console.log("ready!");
+    var $container = null;
 
     $('#search-form').on('submit', function(event) {
         event.preventDefault();
 
         var url = $('#search-url').val();
 
-        console.log(url);
+        $.ajax({
+            url: '/loader.php?url=' + url
+        }).done(function(response) {
+            $container = $(response);
+            processWebsite(url);
+        });
     });
+
+    function processWebsite(url) {
+        $line = $('<tr></tr>')
+
+
+
+
+        $('#result-url').html(url);
+
+        $line.append('<td>' + url + '</td>');
+        $line.append(addCounterTableCell($container, 'a'));
+        $line.append(addCounterTableCell($container, 'span'));
+        $line.append(addCounterTableCell($container, 'div'));
+        $line.append(addCounterTableCell($container, 'h1'));
+        $line.append(addCounterTableCell($container, 'h2'));
+        $line.append(addCounterTableCell($container, 'h3'));
+        $line.append(addCounterTableCell($container, 'h4'));
+        $line.append(addCounterTableCell($container, 'h5'));
+        $line.append(addCounterTableCell($container, 'h6'));
+        $line.append(addCounterTableCell($container, 'img'));
+
+        $('#result-table').append($line);
+    }
+
+    function addCounterTableCell($container, name) {
+        $td = $('<td></td>');
+
+        $td.html($container.find(name).length);
+        return $td;
+    }
 });
