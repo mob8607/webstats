49f487d865d7c3f8e2532782545a27e2d9e133f5
mob8607 <mathias.ober@massiveart.com>
2015-08-20 00:45:55 +0200

    added bower components, updated table for stats

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -15,17 +15,21 @@ $(document).ready(function() {
     });
 
     function processWebsite(url) {
-        $line = $('<tr></tr>')
-
-
-
+        var $line = $('<tr></tr>')
 
         $('#result-url').html(url);
 
         $line.append('<td>' + url + '</td>');
         $line.append(addCounterTableCell($container, 'a'));
-        $line.append(addCounterTableCell($container, 'span'));
         $line.append(addCounterTableCell($container, 'div'));
+        $line.append(addCounterTableCell($container, 'p'));
+        $line.append(addCounterTableCell($container, 'span'));
+        $line.append(addCounterTableCell($container, 'article'));
+        $line.append(addCounterTableCell($container, 'section'));
+        $line.append(addCounterTableCell($container, 'header'));
+        $line.append(addCounterTableCell($container, 'footer'));
+        $line.append(addCounterTableCell($container, 'aside'));
+        $line.append(addCounterTableCell($container, 'nav'));
         $line.append(addCounterTableCell($container, 'h1'));
         $line.append(addCounterTableCell($container, 'h2'));
         $line.append(addCounterTableCell($container, 'h3'));
@@ -33,6 +37,8 @@ $(document).ready(function() {
         $line.append(addCounterTableCell($container, 'h5'));
         $line.append(addCounterTableCell($container, 'h6'));
         $line.append(addCounterTableCell($container, 'img'));
+        $line.append(addCounterTableCell($container, 'b'));
+        $line.append(addCounterTableCell($container, 'i'));
 
         $('#result-table').append($line);
     }
