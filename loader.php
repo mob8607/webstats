f6303254fed0be80109eb0911135b888cbaf98c6
mob8607 <mathias.ober@massiveart.com>
2015-08-20 00:30:52 +0200

    added follow redirects

--- loader.php
+++ loader.php
@@ -7,8 +7,10 @@ function get_data($url) {
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     $data = curl_exec($ch);
     curl_close($ch);
+
     return $data;
 }
 
